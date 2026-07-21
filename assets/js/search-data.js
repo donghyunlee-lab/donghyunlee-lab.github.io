0a1e718189501ee1d522d41072902e7ef918a370
donghyunlee-ai <donghyunlee.ai@gmail.com>
2026-07-21 05:37:13 +0000

    Deploying to gh-pages from @ donghyunlee-lab/donghyunlee-lab.github.io@fd0293ce2ff286b32b666efccb95fa6b371ba3f1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -120,10 +120,12 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2025-10-25-paper-jcp-xai/";
-            },},{id: "news-now-recruiting-undergraduate-and-graduate-students-our-lab-is-actively-recruiting-passionate-students-for-the-following-positions-graduate-students-master-s-amp-amp-ph-d-we-are-looking-for-highly-motivated-candidates-to-join-our-master-s-or-ph-d-programs-undergraduate-researchers-we-welcome-undergraduate-students-3rd-year-or-higher-interested-in-gaining-research-experience-especially-those-considering-the-integrated-bachelor-s-master-s-program-or-master-s-program-research-areas-ai-mlops-data-science-ideal-candidate-smart-social-integrity-required-skills-python-preferred-skills-front-back-end-development-how-to-apply-please-send-your-cv-and-cover-letter-to-donghyun-lee-at-hufs-ac-kr-please-be-sure-to-include-your-gpa-in-your-application-materials",
-          title: 'Now Recruiting: Undergraduate and Graduate Students Our lab is actively recruiting passionate students...',
+            },},{id: "news-recruiting-now-recruiting-undergraduate-and-graduate-students",
+          title: '📢 [Recruiting] Now Recruiting Undergraduate and Graduate Students',
           description: "",
-          section: "News",},{id: "news-seminar-sungbum-park-us-big-tech-on-agentic-ai-amp-cloud",
+          section: "News",handler: () => {
+              window.location.href = "/news/2025-11-10-recruit-open-positions/";
+            },},{id: "news-seminar-sungbum-park-us-big-tech-on-agentic-ai-amp-cloud",
           title: '🚀 [Seminar] Sungbum Park (US Big Tech) on Agentic AI &amp;amp; Cloud',
           description: "",
           section: "News",handler: () => {
@@ -183,6 +185,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2026-07-07-prof-lee-kosime-vice-chair/";
+            },},{id: "news-recruiting-undergraduate-research-students-up-to-2-apply-by-july-31",
+          title: '📢 [Recruiting] Undergraduate Research Students (Up to 2, Apply by July 31)',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2026-07-21-recruit-undergrad/";
             },},{id: "projects-ai-for-algal-bloom-forecasting",
           title: 'AI for Algal Bloom Forecasting',
           description: "Developing a Trustworthy AI model to predict harmful algal blooms (HABs).",
